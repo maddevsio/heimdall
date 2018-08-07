48c1fa5949ce546300e8fdaf4692839d56d0f841
Aleksandr Sobolev <thesobolev@gmail.com>
2018-08-08 02:19:15 +0600

    Added main:run function for gunicorn call

--- scanner/main.py
+++ scanner/main.py
@@ -7,6 +7,12 @@ def setup_routes(app):
     app.router.add_get('/', scan)
     app.router.add_get('/badge', badge_view)
 
-app = web.Application()
-setup_routes(app)
-web.run_app(app)
+if __name__ == '__main__':
+    app = web.Application()
+    setup_routes(app)
+    web.run_app(app)
+
+async def run():
+    app = web.Application()
+    setup_routes(app)
+    return app
