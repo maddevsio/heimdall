dff4962c40dcba065ff586a18af831338bc3816d
Эрик Султаналиев <ersul4ik@mail.ru>
2018-08-30 10:12:23 +0600

    Changed functions, removed extra lines of code

--- scanner/generate_byte_code.py
+++ scanner/generate_byte_code.py
@@ -5,12 +5,14 @@ from solc import compile_source
 
 def convert_to_byte_code(file, contract_name):
     report = {}
-    compiled_sol = compile_source(open(file, mode='r').read())
+    with open(file, 'r') as f:
+        content = f.read()
+    compiled_sol = compile_source(content)
     if type(contract_name) is list:
         for contract in contract_name:
-            contract_interface = compiled_sol['<stdin>:{}'.format(str(contract))]
-            report[str(contract)] = {'bytecode': contract_interface['bin'], 'abi': compiled_sol['<stdin>:' +
-                                                                                                str(contract)]['abi']}
+            contract_interface = compiled_sol[str(contract)]
+            report[str(contract).split('<stdin>:')[1]] = {'bytecode': contract_interface['bin'],
+                                                          'abi': compiled_sol[str(contract)]['abi']}
         print(report)
     else:
         contract_interface = compiled_sol['<stdin>:{}'.format(str(contract_name))]
@@ -21,11 +23,10 @@ def convert_to_byte_code(file, contract_name):
 
 
 def get_contract_names(file):
-    path_to_file = open(file, mode='r')
-    compiled_sol = compile_source(path_to_file.read())
-
-    name_of_contract = compiled_sol['<stdin>:SafeMath']['ast']['attributes']['exportedSymbols']
-    return convert_to_byte_code(file, contract_name=list(name_of_contract.keys()))
+    with open(file, 'r') as f:
+        content = f.read()
+    compiled_sol = compile_source(content)
+    return convert_to_byte_code(file, contract_name=list(compiled_sol.keys()))
 
 
 @click.command()
@@ -35,9 +36,8 @@ def get_contract_names(file):
 def main(solidity_file, t, contract):
     if t == 'bytecode':
         if not contract:
-            get_contract_names(solidity_file)
-        else:
-            convert_to_byte_code(solidity_file, contract)
+            return get_contract_names(solidity_file)
+        convert_to_byte_code(solidity_file, contract)
 
 
 if __name__ == '__main__':
