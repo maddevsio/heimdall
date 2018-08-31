5a0e33b7730bbc1e1cb0d3a20fefa64d6407b77d
Эрик Султаналиев <ersul4ik@mail.ru>
2018-08-31 14:01:30 +0600

    added correct paylod for requests params

--- scanner/get_from_contract_address.py
+++ scanner/get_from_contract_address.py
@@ -2,16 +2,27 @@ import requests
 import json
 import click
 
-def get_bytecode(address):
-    endpoint = 'https://api.infura.io/v1/jsonrpc/mainnet/eth_getCode' + \
-                            '?params=["%s", "latest"]' % address
-    response = requests.get(endpoint).json()
-    return response['result']
+URL = 'https://api.infura.io/v1/jsonrpc/'
+METHOD  =  'eth_getCode' 
+
+
+def get_bytecode(address, network):
+    NETWORK = network + '/'
+    endpoint = URL + NETWORK + METHOD 
+    payload = {'params': json.dumps(["{}".format(address), "latest"])}
+
+    response = requests.get(endpoint, params=payload)
+    if 400 <= response.status_code < 404:
+        return print("No data was found")
+
+    return response.json()['result']
+
 
 @click.command()
 @click.option('--address', '-a',  required=True, help="Set address to get the bytecode")
-def main(address):
-    bytecode = get_bytecode(address)
+@click.option('--network', '-n',  required=True, help="Set network")
+def main(address, network):
+    bytecode = get_bytecode(address, network)
     print(bytecode)
 
 if __name__ == '__main__':
