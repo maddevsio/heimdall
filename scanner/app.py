595a4b3bcd6059e87f333be54a78ec88010fe32f
Эрик Султаналиев <ersul4ik@mail.ru>
2018-09-28 11:30:44 +0600

    added env variables to project

--- scanner/app.py
+++ scanner/app.py
@@ -194,5 +194,10 @@ async def report_view_json(request):
 @aiohttp_jinja2.template('worker_view.jinja2')
 async def worker_view(request):
     return {
-        'status': 'running'
+        'apiKey': os.environ.get('apiKey'),
+        'authDomain': os.environ.get('authDomain'),
+        'databaseURL': os.environ.get('databaseURL'),
+        'projectId': os.environ.get('projectId'),
+        'storageBucket': os.environ.get('storageBucket'),
+        'messagingSenderId': os.environ.get('messagingSenderId'),
     }
